8479d907961eac52d2cfe6f1f861194c4a1b7034
Dominic Scheirlinck <dominic@varspool.com>
2016-08-18 16:32:23 +1200

    Specify in ms

--- tests/curl-php/run.php
+++ tests/curl-php/run.php
@@ -7,8 +7,8 @@ if (empty($_SERVER['TEST_HOSTNAME'])) {
 
 $ch = curl_init(sprintf('http://%s/', $_SERVER['TEST_HOSTNAME']));
 
-curl_setopt($ch, CURLOPT_TIMEOUT, 5000);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3000);
+curl_setopt($ch, CURLOPT_TIMEOUT_MS, 5000);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 3000);
 
 curl_exec($ch);
 curl_close($ch);
